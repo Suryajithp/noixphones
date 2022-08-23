dffe58b1b0ee0c6bb4b51d775552699243c9b248
Suryajithp <suryajithp2000@gmail.com>
2022-08-23 16:59:08 +0530

    first update

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 } 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+   const url='mongodb+srv://suryajith:Mongodb@noixphone.jbecpei.mongodb.net/?retryWrites=true&w=majority'
     const dbname='galexy'
 
     mongoClient.connect(url,(err,data)=>{
--- routes/user.js
+++ routes/user.js
@@ -27,7 +27,7 @@ router.get('/', async (req, res, next) => {
       cartCount = await userHelpers.getCartcount(user1._id)
       wishlist = await userHelpers.getWishlistProducts(user1._id)
     }
-    let banner = await productHelpers.getAllBanner()
+    // let banner = await productHelpers.getAllBanner()
 
     productHelpers.getAllProducts().then((products) => {
       if (wishlist >= 1) {
@@ -39,7 +39,8 @@ router.get('/', async (req, res, next) => {
           }
         }
       }
-      res.render('index', { products, user: true, user1, cartCount, banner, wishlist })
+      // banner,
+      res.render('index', { products, user: true, user1, cartCount, wishlist })
     })
   } catch (error) {
     console.log(error);
